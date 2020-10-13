19a937d416590197ff567f30ffa24f9129350092
Fuzeless <lequangnhat2000@gmail.com>
2020-10-13 17:46:10 +0700

    Update template-processor.js

--- week6-javascript/lab5/template-processor.js
+++ week6-javascript/lab5/template-processor.js
@@ -4,11 +4,17 @@ class TemplateProcessor {
   }
 
   fillIn(dictionary) {
-    // let returnString = template.replace(/{{day}}/g, dictionary.day).replace(/{{month}}/g, dictionary.month).replace(/{{year}}/g, dictionary.year);
     let returnString = template;
-    for (let key in dictionary) {
-      returnString = returnString.replace(new RegExp(`{{${key}}}`), dictionary[key]);
-    }
-    return returnString.replace(/{{\S*}}/g, "");
+    // let returnString = template.replace(/{{day}}/g, dictionary.day).replace(/{{month}}/g, dictionary.month).replace(/{{year}}/g, dictionary.year);
+    // for (let key in dictionary) {
+    //   returnString = returnString.replace(new RegExp(`{{${key}}}`), dictionary[key]);
+    // }
+    // return returnString.replace(/{{\S*}}/g, "");
+    let regex = /{{\S+}}/g;
+    template.match(regex).forEach(e => {
+      let key = e.replace(/^{{/, "").replace(/}}$/, "");
+      returnString = returnString.replace(new RegExp(e,'g'), dictionary[key] ? dictionary[key] : "");
+    })
+    return returnString;
   }
 }
